010b0fe78802cf7a61a23d85da5b7fd8208eb7cc
BhumikaR2586 <102bhumika11scia@gmail.com>
2025-03-26 14:58:24 +0530

    add space

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -7,7 +7,7 @@ public class CameraFollow : MonoBehaviour
 
     void LateUpdate()
     {
-        Vector3 targetPosition = new Vector3(player.position.x, transform.position.y, transform.position.z);
+        Vector3 targetPosition = new  Vector3(player.position.x, transform.position.y, transform.position.z);
         transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeed * Time.deltaTime);
     }
 }
